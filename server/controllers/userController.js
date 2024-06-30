267b876df7400c4bcc50c902d949b1b86df2f66c
Rahul S <rahulsreekumar03@gmail.com>
2024-06-30 11:54:54 +0530

    cross site cookie enabled

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,7 +16,7 @@ const loginController = async (req, res) => {
                 res.cookie('token', token,
                     {
                         httpOnly: true,
-                        sameSite: 'Strict',
+                        sameSite: None,
                         maxAge: 60 * 60 * 1000,
                         secure: process.env.NODE_ENV === 'production',
                         path: '/',
